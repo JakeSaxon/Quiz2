9a4f3e57406a9a63f1fa9d9b298ddef942fe68b3
Jacob Saxon <Jake@Jacobs-MacBook-Pro-3.local>
2016-10-26 15:33:11 -0400

    10/26/16

---
+++ Tests.py
@@ -0,0 +1,11 @@
+
+import unittest
+
+class HomePageTest(unittest.TestCase):
+
+    def SetUp(self):
+        if os.name=='nt'
+            self.browser = webdriver.Chrome()
+
+        else:
+            self.browser = webdriver.Firefox()
